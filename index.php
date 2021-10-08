90958305f457cf2c18f9e63821e1686b47ba866c
Kalyugin Oleg <oleg.kalyugin2001@mail.ru>
2021-10-08 17:26:40 +0200

    Index: Изменение содержимого файла

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <?php
 
-$a = 4;
-$b = 6;
-
-$c = test($a, $b);
-echo $c;
-
-function test($a, $b)
+$socket = fsockopen("test.shelenkov.com", 80, $errorCode, $errorString);
+if (!$socket)
 {
-    $c = $a + $b;
-
-    return $c;
+	echo "$errorCode ($errorString)<br />\n";
+	die();
 }
+
+$result  = "" . "\r\n";
+$result .= "" . "\r\n";
+$result .= "" . "\r\n";
+$result .= "\r\n";
+
+fwrite($socket, $result);
+while (!feof($socket))
+{
+	echo fgets($socket);
+}
+fclose($socket);
+
+
+
+
+
+
+
+
+
