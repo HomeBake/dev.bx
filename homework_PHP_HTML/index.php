5dc1bbcf4d6434c16ca91fa7c2620fe872fdf483
Kalyugin Oleg <oleg.kalyugin2001@mail.ru>
2021-11-22 22:10:51 +0200

    index.php: сохраниение результата подключения бд в переменную, вызов функции получения жанров movie-info.php: сохраниение результата подключения бд в переменную, вызов функции получения актёров.

--- homework_PHP_HTML/index.php
+++ homework_PHP_HTML/index.php
@@ -1,18 +1,23 @@
 <?php
 
 /** @var array $page_config */
+/** @var array $bd_config */
 /** @var array $genres */
 /** @var array $movies */
 
 declare(strict_types = 1);
 require_once "./config/config.php";
-require_once "./lib/template-functions.php";
-require_once "./lib/movies-info.php";
-require_once "./lib/movie-functions.php";
-require_once "./lib/selected-page.php";
+require_once "./lib/functions/bd-function.php";
+require_once "./lib/functions/template-functions.php";
+require_once "./lib/functions/movie-functions.php";
+require_once "./lib/functions/connect-to-bd.php";
+require_once "./lib/functions/selected-page.php";
+require_once "./lib/functions/refactor_query.php";
 
-$page = renderTemplate("./resources/pages/main.php", ['movies' => getMovies($movies,$genres)]);
-
+$database = connectToBd($bd_config);
+$genres = getAll($database,'genre');
+$page = renderTemplate("./resources/pages/main.php", ['movies' => getMovies($database,$genres,$_GET['genre'])]);
+/*$moviese = getMovies($database,$genres,$_GET['genre']);*/
 
 
 renderLayout($page, [
@@ -20,6 +25,6 @@ renderLayout($page, [
 	'genres'=> $genres,
 	'homeId' => $page_config['menu']['home'],
 	'favoriteId' =>$page_config['menu']['favorite'],
-	'selectedPage' => selectedPage()
-]);
+	'selectedPage' => selectedPage(),
 
+]);
--- homework_PHP_HTML/movie-info.php
+++ homework_PHP_HTML/movie-info.php
@@ -1,23 +1,28 @@
 <?php
 /** @var array $page_config */
-/** @var array $genres */
-/** @var array $movies */
+/** @var array $bd_config */
 
 declare(strict_types = 1);
 require_once "./config/config.php";
-require_once "./lib/template-functions.php";
-require_once "./lib/movies-info.php";
-require_once "./lib/movie-functions.php";
-require_once "./lib/selected-page.php";
+require_once "./lib/functions/bd-function.php";
+require_once "./lib/functions/template-functions.php";
+require_once "./lib/functions/movie-functions.php";
+require_once "./lib/functions/selected-page.php";
+require_once "./lib/functions/connect-to-bd.php";
+require_once "./lib/functions/refactor_query.php";
 
 
+$database = connectToBd($bd_config);
+$actors = getAll($database,'actor');
 $page = renderTemplate("./resources/pages/info.php", [
-	'movies' => getMovieByID($_GET['movie'],$movies)
+	'movies' => getMovieByID($database,$actors,$_GET['movie'])
 ]);
+
+$database = connectToBd($bd_config);
 
 renderLayout($page, [
 	'config' => $page_config,
-	'genres'=> $genres,
+	'genres'=> getAll($database,'genre'),
 	'homeId' => $page_config['menu']['home'],
 	'favoriteId' =>$page_config['menu']['favorite'],
 	'selectedPage' => selectedPage()
